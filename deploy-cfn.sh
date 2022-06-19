319a43887e622655ca17f2c8d7758ce7229eba22
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-18 22:49:59 -0400

    Renamed files

--- deploy-cfn.sh
+++ deploy-cfn.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "**THIS SCRIPT VALIDATES AND CREATES/UPDATES THE CF STACK**"
+echo "This script valdates a CloudFormation template, and creates/updates a CloudFormation Stack"
 
-aws cloudformation deploy --stack-name ec2-stack --template-file ec2.yml --parameter-overrides file://parameters/ec2.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM #--no-execute-changeset
+RanNumGen=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+
+aws cloudformation deploy \
+    --stack-name cfn-stack-$1-$RanNumGen \
+    --template-file $1.yml \
+    --parameter-overrides file://parameters/$1.json \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_IAM \
+    #--no-execute-changeset
