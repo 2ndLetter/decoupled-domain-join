1b63b87d010c73e85b86505ff8a0f56b029fb5b7
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-18 22:57:18 -0400

    WIP

--- deploy-cfn.sh
+++ deploy-cfn.sh
@@ -2,10 +2,10 @@
 
 echo "This script valdates a CloudFormation template, and creates/updates a CloudFormation Stack"
 
-RanNumGen=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+RanNumGen=-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 
 aws cloudformation deploy \
-    --stack-name cfn-stack-$1-$RanNumGen \
+    --stack-name cfn-stack-$1$RanNumGen \
     --template-file $1.yml \
     --parameter-overrides file://parameters/$1.json \
     --no-fail-on-empty-changeset \
