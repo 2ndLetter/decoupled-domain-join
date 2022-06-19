cd55ecacf167219fd110f8dfecdbc251a59a5962
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-18 22:25:04 -0400

    working stack

--- deploy-cfn.sh
+++ deploy-cfn.sh
@@ -2,4 +2,4 @@
 
 echo "**THIS SCRIPT VALIDATES AND CREATES/UPDATES THE CF STACK**"
 
-aws cloudformation deploy --stack-name temp --template-file ec2.yml --parameter-overrides file://parameters/ec2.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM #--no-execute-changeset
+aws cloudformation deploy --stack-name ec2-stack --template-file ec2.yml --parameter-overrides file://parameters/ec2.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM #--no-execute-changeset
