8ccfc8599ac2d49a7dd0c36dbcdb7f9cc6ab1dc2
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-19 12:22:42 -0400

    Added some additional functionality

--- deploy-cfn.sh
+++ deploy-cfn.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-while getopts 'nr:' OPTION; do
+while getopts 'n:rc' OPTION; do
   case "$OPTION" in
     n)
       argN="$OPTARG"
-      echo "Option n used with: $argN"
       ;;
     r)
       RanNumGen=-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
       ;;
+    c)
+      argC="--no-execute-changeset"
+      ;;
     ?)
-      echo "Usage: $(basename $0) [-n argument] [-r]"
+      echo "Usage: $(basename $0) [-n argument] [-r] [-c]"
       exit 1
       ;;
   esac
 done
 
-
 echo "This script valdates a CloudFormation template, and creates/updates a CloudFormation Stack"
-
-#RanNumGen=-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 
 aws cloudformation deploy \
     --stack-name cfn-stack-$argN$RanNumGen \
@@ -27,4 +26,4 @@ aws cloudformation deploy \
     --parameter-overrides file://parameters/$argN.json \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
-    #--no-execute-changeset
+    $argC
