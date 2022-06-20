6eefef16877953f60f0dd80f932c56c1a105d8de
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-20 16:05:02 -0400

    WIP

--- keypair.sh
+++ keypair.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -xe
 
+# Remove old keypair
+rm -fr ~/.ssh/$1.pem ~/.ssh/$1.pub
+
 # Create keypair in aws account and local ~/.ssh folder
 aws ec2 create-key-pair --key-name $1 --query "KeyMaterial" --output text > ~/.ssh/$1.pem
 
 chmod 400 ~/.ssh/$1.pem
 
-aws ec2 describe-key-pairs
+#aws ec2 describe-key-pairs
