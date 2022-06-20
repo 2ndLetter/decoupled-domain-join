a8b5d0afde71b94bb29c04c1c904cd6bf04caaf8
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-20 16:44:59 -0400

    Updated deploy-cfn.sh

--- deploy-cfn.sh
+++ deploy-cfn.sh
@@ -2,19 +2,21 @@
 
 #Todo: Script doesn't work when choosing a specific stack name
 
-while getopts 'n:rc' OPTION; do
+while getopts 'n:u:c' OPTION; do
   case "$OPTION" in
     n)
       argN="$OPTARG"
+      NAME=$argN-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
       ;;
-    r)
-      RanNumGen=-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+    u)
+      argU="$OPTARG"
+      NAME=$argN-$argU
       ;;
     c)
       argC="--no-execute-changeset"
       ;;
     ?)
-      echo "Usage: $(basename $0) [-n argument] [-r] [-c]"
+      echo "Usage: $(basename $0) [-n argument] [-u argument] [-c]"
       exit 1
       ;;
   esac
@@ -26,10 +28,16 @@ then
   exit 1
 fi
 
+echo $NAME
+exit 0
+
+#NAME=$argN$RanNumGen
+
 echo "This script valdates a CloudFormation template, and creates/updates a CloudFormation Stack"
 
 aws cloudformation deploy \
-    --stack-name cfn-stack-$argN$RanNumGen \
+    #--stack-name cfn-stack-$argN$argR \
+    --stack-name cfn-stack-$NAME
     --template-file $argN.yml \
     --parameter-overrides file://parameters/$argN.json \
     --no-fail-on-empty-changeset \
