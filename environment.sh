d7744b5e14cb688d9f4996183dc161b09f967bb3
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-23 22:32:01 -0400

    added enviornment parameter file configuration to script

--- environment.sh
+++ environment.sh
@@ -5,7 +5,18 @@ rm -fr ~/.ssh/$1.pem ~/.ssh/$1.pub
 
 # Create keypair in aws account and local ~/.ssh folder
 aws ec2 create-key-pair --key-name $1 --query "KeyMaterial" --output text > ~/.ssh/$1.pem
-
 chmod 400 ~/.ssh/$1.pem
 
-#aws ec2 describe-key-pairs
+# Return VPC
+VPC=$(aws ec2 describe-vpcs | jq -r '.Vpcs[]' | jq -r .VpcId)
+
+# Return Subnets
+SUBNET_A=$(aws ec2 describe-subnets | jq -r '.Subnets[0]' | jq -r '.SubnetId')
+SUBNET_B=$(aws ec2 describe-subnets | jq -r '.Subnets[1]' | jq -r '.SubnetId')
+
+# Configure parameters file
+cp environment_template.json parameters/environment.json
+
+sed -i "s/VPC_ID/$VPC/" parameters/environment.json
+sed -i "s/SUBNET_A/$SUBNET_A/" parameters/environment.json
+sed -i "s/SUBNET_B/$SUBNET_B/" parameters/environment.json
