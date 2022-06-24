7a5f3b72d75e865a22cedc2897e3fd88a5534579
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-06-23 22:07:18 -0400

    WIP

--- ssh.py
+++ ssh.py
@@ -3,16 +3,13 @@
 import paramiko
 
 # Userdata script:
-# - Returns the private ip address
+# - Returns the private ip address and S3 bucket name
 # - Invokes the lambda function (payload is private ip address, S3 bucket)
 # Lambda Function:
-# - Retrieve private ip address from Userdata
-# - Retrieve private S3 bucket from Userdata
-# - Retrieve private ssh key from S3 bucket
-# - Retrieve AD credentials from S3 bucket
+# - Retrieve private ip address and S3 bucket name from payload
+# - Retrieve private ssh key and AD credentials from S3 bucket
 # - SSH to server using private ip address
 # - Run domain join command
-
 
 #########################
 
@@ -20,20 +17,17 @@ k = paramiko.RSAKey.from_private_key_file("/home/bmchadwick/.ssh/bootstrap")
 c = paramiko.SSHClient()
 c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print("connecting")
-c.connect( hostname = "44.207.146.64", username = "bootstrap", pkey = k )
+c.connect( hostname = "34.232.176.208", username = "bootstrap", pkey = k )
 print("connected")
 commands = [ "echo \"P@\$\$Word123\" | sudo realm join -v -U admin lab.example.com", "date" ]
-#commands = [ "echo \"P@\$\$Word123\"", "date" ]
+#commands = [ "echo \"P@\$\$Word123\" | sudo realm leave -v -U admin lab.example.com", "date" ]
 for command in commands:
-	print("Executing {}".format( command ))
-	stdin , stdout, stderr = c.exec_command(command)
-	print(stdout.read())
-	print( "Errors")
-	print(stderr.read())
+    print("Executing {}".format( command ))
+    stdin , stdout, stderr = c.exec_command(command)
+    print(stdout.read())
+    print( "Errors")
+    print(stderr.read())
 c.close()
-
-
-
 
 #USERNAME_GET = (output['UserName'])
 #PASSWORD_GET = (output['Password'])
