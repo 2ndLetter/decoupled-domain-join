6fc14f2c678d48883a215db5a614df70eeae853d
Brian <b.matthew.chadwick@gmail.com>
2022-06-25 09:32:34 -0400

    working

--- s3_lambda.py
+++ s3_lambda.py
@@ -4,12 +4,12 @@ import json
 
 def lambda_handler(event, context):
     
-    def get_passwords():
+    #print(event['aws_region'])
+    #print(event['s3_object_pw'])
+    #print(event['s3_bucket_name'])
     
-        #print(event['aws_region'])
-        #print(event['s3_object_pw'])
-        #print(event['s3_bucket_name'])
-        
+    def get_passwords():
+
         s3_resource = boto3.resource("s3", region_name=event['aws_region'])
         s3_object = s3_resource.Object(event['s3_bucket_name'], event['s3_object_pw'])
         
@@ -20,8 +20,6 @@ def lambda_handler(event, context):
             print(json_object["password"])
             
     def get_ssh_key():
-        #AWS_REGION = "us-east-1"
-        #S3_BUCKET_NAME = "cfn-stack-environment-s3bucket-1pq7lgfwwy8qj"
         s3_resource = boto3.resource("s3", region_name=event['aws_region'])
         s3_object = s3_resource.Object(event['s3_bucket_name'], event['s3_object_k'])
         with io.BytesIO() as f:
@@ -31,9 +29,4 @@ def lambda_handler(event, context):
         
     get_passwords()
     get_ssh_key()
-    
-    #message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])
-    #message = 'Hello {} {} {}!'.format(event['first_name'], event['last_name'], event['aws_region'])  
-    #return { 
-    #    'message' : message
-    #}
+
