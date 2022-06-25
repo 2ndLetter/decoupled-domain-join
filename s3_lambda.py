3f8fd3653f772bbb426e378a0a6af7daa7daba83
Brian <b.matthew.chadwick@gmail.com>
2022-06-25 08:32:45 -0400

    WIP

--- s3_lambda.py
+++ s3_lambda.py
@@ -3,10 +3,10 @@ import boto3
 import json
 
 def lambda_handler(context, event):
-    
-    def get_passwords():      
+
+    def get_passwords():
         AWS_REGION = "us-east-1"
-        S3_BUCKET_NAME = "cfn-stack-environment-s3bucket-6lz191zpubr"
+        S3_BUCKET_NAME = "cfn-stack-environment-s3bucket-1pq7lgfwwy8qj"
         s3_resource = boto3.resource("s3", region_name=AWS_REGION)
         s3_object = s3_resource.Object(S3_BUCKET_NAME, 'passwords.json')
         with io.BytesIO() as f:
@@ -14,16 +14,16 @@ def lambda_handler(context, event):
             f.seek(0)
             json_object = json.loads(f.read())
             print(json_object["password"])
-            
+
     def get_ssh_key():
         AWS_REGION = "us-east-1"
-        S3_BUCKET_NAME = "cfn-stack-environment-s3bucket-6lz191zpubr"
+        S3_BUCKET_NAME = "cfn-stack-environment-s3bucket-1pq7lgfwwy8qj"
         s3_resource = boto3.resource("s3", region_name=AWS_REGION)
         s3_object = s3_resource.Object(S3_BUCKET_NAME, 'bootstrap')
         with io.BytesIO() as f:
             s3_object.download_fileobj(f)
             f.seek(0)
             print(f.read())
-    
+
     get_passwords()
     get_ssh_key()
