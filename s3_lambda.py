45700cc3a5ebb269483b42d4305712149733befd
Brian <b.matthew.chadwick@gmail.com>
2022-06-25 12:19:49 -0400

    WIP

--- s3_lambda.py
+++ s3_lambda.py
@@ -3,22 +3,20 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    
+
     #print(event['aws_region'])
     #print(event['s3_object_pw'])
     #print(event['s3_bucket_name'])
-    
-    def get_passwords():
 
+    def get_passwords():
         s3_resource = boto3.resource("s3", region_name=event['aws_region'])
         s3_object = s3_resource.Object(event['s3_bucket_name'], event['s3_object_pw'])
-        
         with io.BytesIO() as f:
             s3_object.download_fileobj(f)
             f.seek(0)
             json_object = json.loads(f.read())
             print(json_object["password"])
-            
+
     def get_ssh_key():
         s3_resource = boto3.resource("s3", region_name=event['aws_region'])
         s3_object = s3_resource.Object(event['s3_bucket_name'], event['s3_object_k'])
@@ -26,7 +24,6 @@ def lambda_handler(event, context):
             s3_object.download_fileobj(f)
             f.seek(0)
             print(f.read())
-        
+
     get_passwords()
     get_ssh_key()
-
