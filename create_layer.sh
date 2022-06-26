a6c7526ff08c3b1d227317c470218041f9444c23
Brian <b.matthew.chadwick@gmail.com>
2022-06-26 06:50:01 -0400

    create_layer.sh working

--- create_layer.sh
+++ create_layer.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -e
 
 # Run from within Cloud9 (or an AWS Linux 2 ec2 instance)
-# Create within a python virtual environment
 
+echo "Create (and activate) a python virtual environment"
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Create python folder"
+mkdir python
+
+echo "Create Array of python packages installed via `pip install paramiko`"
 packageArray=("bcrypt" "cffi" "cryptography" "paramiko" "pycparser" "PyNaCl" "six")
 
+echo "Install each package as per aws documentation"
+# https://aws.amazon.com/premiumsupport/knowledge-center/lambda-python-package-compatible/
 for i in ${packageArray[@]};
 do
-  echo "Packaging $i"
+  echo "Packaging $i !!"
+  sleep 2
   pip install \
       --platform manylinux2014_x86_64 \
       --target=python \
@@ -16,3 +26,13 @@ do
       --only-binary=:all: --upgrade \
       $i
 done
+
+echo "Zip up packages for upload/import"
+zip -r python.zip python/
+
+echo "Upload to s3"
+S3Bucket=$(aws s3 ls | grep cfn-stack | awk '{print $NF}')
+aws s3 cp python.zip s3://$S3Bucket
+
+echo "deactivate venv"
+deactivate
